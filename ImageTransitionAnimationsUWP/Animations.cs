35b17e40fd6dc6dc6feabdf070bd325af156bc87
Oren Nachman <code@nachmore.com>
2020-06-04 00:44:35 +1000

    Spring animations no longer get stuck
    Spring animations were using the Period property incorrectly (it doesn't
    define animation duration, but rather oscillation duration).

--- ImageTransitionAnimationsUWP/Animations.cs
+++ ImageTransitionAnimationsUWP/Animations.cs
@@ -6,6 +6,11 @@ namespace ImageTransitionAnimationsUWP
 {
   public class Animations
   {
+    // based on the recommendations from:
+    // https://docs.microsoft.com/el-gr/windows/uwp/composition/spring-animations
+    private const float SPRING_DAMPING_RATIO = 0.7f;
+    private const double SPRING_PERIOD_MS = 50;
+
     private readonly Compositor compositor;
 
     public Animations(Compositor compositor)
@@ -28,11 +33,17 @@ namespace ImageTransitionAnimationsUWP
     public CompositionAnimation CreateScaleSpringAnimation(Vector3 initialValue, Vector3 finalValue, TimeSpan duration)
     {
       var animation = compositor.CreateSpringVector3Animation();
+
+      animation.Target = "Scale";
+
       animation.InitialValue = initialValue;
       animation.FinalValue = finalValue;
-      animation.Period = duration;
+
+      animation.Period = TimeSpan.FromMilliseconds(SPRING_PERIOD_MS);
+      animation.DampingRatio = SPRING_DAMPING_RATIO;
+
       animation.StopBehavior = AnimationStopBehavior.SetToFinalValue;
-      animation.Target = "Scale";
+
       return animation;
     }
 
@@ -51,10 +62,15 @@ namespace ImageTransitionAnimationsUWP
     public CompositionAnimation CreateOpacitySpringAnimation(float startValue, float endValue, TimeSpan duration)
     {
       var animation = compositor.CreateSpringScalarAnimation();
+
       animation.InitialValue = startValue;
-      animation.StopBehavior = AnimationStopBehavior.SetToFinalValue;
-      animation.Period = duration;
       animation.FinalValue = endValue;
+
+      animation.Period = TimeSpan.FromMilliseconds(SPRING_PERIOD_MS);
+      animation.DampingRatio = SPRING_DAMPING_RATIO;
+
+      animation.StopBehavior = AnimationStopBehavior.SetToFinalValue;
+
       return animation;
     }
 
@@ -73,10 +89,15 @@ namespace ImageTransitionAnimationsUWP
     public CompositionAnimation CreateSlideSpringAnimation(float startOffset, float endOffset, TimeSpan duration)
     {
       var animation = compositor.CreateSpringVector3Animation();
+
       animation.InitialValue = new Vector3(startOffset, 0f, 0f);
       animation.FinalValue = new Vector3(endOffset, 0f, 0f);
+
+      animation.Period = TimeSpan.FromMilliseconds(SPRING_PERIOD_MS);
+      animation.DampingRatio = SPRING_DAMPING_RATIO;
+
       animation.StopBehavior = AnimationStopBehavior.SetToFinalValue;
-      animation.Period = duration;
+      
       return animation;
     }
   }
